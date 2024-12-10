2a5c4645162b013183b2b3147c94f95a16b267f7
nian <1242095876@qq.com>
2024-12-10 20:17:59 +0800

    lcnDemo-init

---
+++ lcn-order/src/main/java/com/nian/lcnorder/LcnOrderApplication.java
@@ -0,0 +1,13 @@
+package com.nian.lcnorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcnOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcnOrderApplication.class, args);
+    }
+
+}
